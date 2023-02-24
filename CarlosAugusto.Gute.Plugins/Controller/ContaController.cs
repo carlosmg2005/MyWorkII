ecc0e7fb9da06e5221c3505ee97e22cf70787053
gute007 <gute007@gmail.com>
2023-02-24 14:03:11 -0300

    Adicionar arquivos de projeto.

---
+++ CarlosAugusto.Gute.Plugins/Controller/ContaController.cs
@@ -0,0 +1,41 @@
+﻿using CarlosAugusto.Gute.MyPlugins.Model;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Rest;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Tooling.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarlosAugusto.Gute.MyPlugins.Controller
+{
+    public class ContaController
+    {
+        public IOrganizationService ServiceClient { get; set; }
+        public Conta Conta { get; set; }
+
+        public ContaController(IOrganizationService crmServiceCliente)
+        {
+            ServiceClient = crmServiceCliente;
+            this.Conta = new Conta(ServiceClient);
+        }
+
+        public ContaController(CrmServiceClient crmServiceCliente)
+        {
+            ServiceClient = crmServiceCliente;
+            this.Conta = new Conta(ServiceClient);
+        }
+
+        public Entity GetAccountById(Guid id, string[] columns)
+        {
+            return Conta.GetAccountById(id, columns);
+        }
+
+        public void IncrementOrDecrementNumberOfOpp(Entity oppAccount, bool? incrementOrDecrement)
+        {
+            Conta.IncrementOrDecrementNumberOfOpp(oppAccount, incrementOrDecrement);
+        }
+    }
+}
---
+++ CarlosAugusto.Gute.Plugins/Model/Conta.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Client;
+using Microsoft.Xrm.Sdk.Deployment;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Tooling.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarlosAugusto.Gute.MyPlugins.Model
+{
+    public class Conta
+    {
+        public IOrganizationService ServiceClient { get; set; }
+
+        public string Logicalname { get; set; }
+
+        public Conta(IOrganizationService crmServiceClient)
+        {
+            this.ServiceClient = crmServiceClient;
+            this.Logicalname = "account";
+        }
+
+        public Entity GetAccountById(Guid id, string[] columns)
+        {
+            return ServiceClient.Retrieve(this.Logicalname, id, new ColumnSet(columns));
+        }
+
+        public void IncrementOrDecrementNumberOfOpp(Entity oppAccount, bool? decrementOrIncrement)
+        {
+            int numberOfOpp = oppAccount.Contains("dcp_nmr_total_opp") ? (int)oppAccount["dcp_nmr_total_opp"] : 0;
+
+            if (Convert.ToBoolean(decrementOrIncrement))
+            {
+                numberOfOpp += 1;  //FAZ PARTE DO CREATE, NAO FOI EXIGIDO NO ENUNCIADO, MAS OPTEI POR DEIXAR
+            }
+            else
+            {
+                numberOfOpp -= 1;
+            }
+            oppAccount["dcp_nmr_total_opp"] = numberOfOpp;
+            ServiceClient.Update(oppAccount);
+        }
+    }
+}
